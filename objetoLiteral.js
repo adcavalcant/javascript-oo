4ba528898775233fa7e8be8455113a7bc33e788c
adcavalcant <aderc19@gmail.com>
2023-08-15 22:19:26 -0300

    alterando a role de user

--- objetoLiteral.js
+++ objetoLiteral.js
@@ -2,7 +2,7 @@ const user = {
   nome: "Juliana",
   email: "j@j.com",
   nascimento: "2021-01-01",
-  role: "admin",
+  role: "estudante",
   ativo: true,
   exibirInfos: function () {
     console.log(this.nome, this.email);
