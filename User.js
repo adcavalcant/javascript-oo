a14ad8629714e56fb6c3fff490a00d324f795877
adcavalcant <aderc19@gmail.com>
2023-08-19 18:17:02 -0300

    Métodos Getters

--- User.js
+++ User.js
@@ -13,6 +13,26 @@ export default class User {
         this.#ativo = ativo;
     }
 
+    get nome(){
+        return this.#nome;
+    }
+
+    get email(){
+        return this.#email;
+    }
+
+    get nascimento(){
+        return this.#nascimento;
+    }
+
+    get role(){
+        return this.#role;
+    }
+
+    get ativo(){
+        return this.#ativo;
+    }
+
     #montaObjUser(){
         return ({
             nome: this.#nome,
@@ -27,4 +47,5 @@ export default class User {
         const objUser = this.#montaObjUser();
         return `${objUser.nome}, ${objUser.email}, ${objUser.nascimento}, ${objUser.role}, ${objUser.ativo}`;
     }
+
 }
--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import Admin from "./Admin.js";
 import Docente from "./Docente.js";
 
 const novoUser = new User("Mariana", "m@m.com", "2021-01-01");
-console.log(novoUser.exibirInfos());
+// console.log(novoUser.exibirInfos());
 
 const novoAdmin = new Admin("Rodrigo", "r@r.com", "2021-01-01");
-console.log(novoAdmin.exibirInfos());
+// console.log(novoAdmin.exibirInfos());
  
+console.log(novoAdmin.nome);
